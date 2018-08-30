147ee8233507c71fdbc2fa7c2377077c34d3f81e
Michael Cramer <michael_cramer@comcast.com>
2018-08-30 11:24:39 -0400

    update jobs list format

--- _includes/tsv-to-md.pl
+++ _includes/tsv-to-md.pl
@@ -1,14 +1,15 @@
 #!/usr/bin/perl
 
+# Title, Date, Location, Job Details, URL
 while(<>) {
     s/\s+$//;
     s/\"//g;
     @f = split /\t/;
     if($f[0] eq "Title") {
-	pop @f;
-	print join("|", @f), "\n";
+	push @jobs, [ map { "**$_**" } @f ];
     } else {
-	print join("|", @f[0..2], sprintf("[%s](%s)", @f[3..4])), "\n";
+	push @jobs, [ @f[0..2], sprintf("[%s](%s)", @f[3..4]), @f[4] ];
     }
 }
 
+print map { join(" | ", @{$_}[0,3]) . "\n" } @jobs
